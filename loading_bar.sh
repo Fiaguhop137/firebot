c79622391d8a59b61e51790169fb2f23619435d5
Fiaguhop137 <eastonzou@gmail.com>
2026-08-31 08:01:04 -0400

    Auto-commit: 2026.08.31.08.01.04

--- loading_bar.sh
+++ loading_bar.sh
@@ -2,8 +2,7 @@
 bar=("⣾" "⣽" "⣻" "⢿" "⡿" "⣟" "⣯" "⣷")
 while true; do
     for i in {0..7}; do
-        clear
-        echo "${bar[$i]} Loading..."
+        printf "$\r{bar[$i]} Loading..."
         sleep 0.125
     done
 done
